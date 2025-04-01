52fb6147b0c5b2ab03bf5879502e9ab425559010
王真 <zhen.wang12@outlook.com>
2025-04-01 17:30:04 +0800

    添加elementPlus

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -14,6 +14,7 @@ defineProps({
       You’ve successfully created a project with
       <a href="https://vite.dev/" target="_blank" rel="noopener">Vite</a> +
       <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
+      <el-button type="danger">dianji</el-button>
     </h3>
   </div>
 </template>
